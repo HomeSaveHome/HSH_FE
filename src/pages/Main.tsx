094dacf70eae7cba18f6964b892c0c7840fe9eb6
wldy4627 <wldy4627@gmail.com>
2025-02-13 17:17:33 +0900

    feat :: mainPage에 Navbar 추가

---
+++ src/pages/Main.tsx
@@ -0,0 +1,12 @@
+import Navbar from '../components/Navbar/Navbar';
+import "../styles/Main.styles.css";
+
+const Main = () => {
+    return (
+        <div className="main-container">
+            <Navbar />
+        </div>
+    );
+};
+
+export default Main;
